32fabc564cfc22ad782612be6217d2486b83a8fe
Vofe09 <nazariikovalev@gmail.com>
2025-06-28 13:03:36 +0500

    0.7

--- api/photos.js
+++ api/photos.js
@@ -2,7 +2,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 import photos from '../data/photos.js';
 
-// Конфигурация из .env
+// Конфигурация Cloudinary через переменные окружения
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key:    process.env.CLOUDINARY_API_KEY,
@@ -20,17 +20,15 @@ export default function handler(req, res) {
   }
 
   // Генерация временных защищённых URL (на 5 минут)
-  const signedUrls = ids.map(publicId => {
-  return cloudinary.url(publicId, {
-    type: "private",
-    resource_type: "image",
-    format: "jpg", // или "png", если твои фото в PNG
-    sign_url: true,
-    secure: true,
-    expires_at: Math.floor(Date.now() / 1000) + 60 * 5
-  });
-});
-
+  const signedUrls = ids.map(publicId =>
+    cloudinary.url(publicId, {
+      type: "private",
+      resource_type: "image",
+      sign_url: true,
+      secure: true,
+      expires_at: Math.floor(Date.now() / 1000) + 60 * 5
+    })
+  );
 
   res.json({ success: true, urls: signedUrls });
 }
